2057fac94e637d7abd75ed341ff9e154a142e539
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-23 11:13:08 -0400

    modified app.js to include count & usestate

--- use_state_initial_demo/src/App.js
+++ use_state_initial_demo/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
 
-  
+  // Declare a new state variable, which we'll call "count" & initialize it to 0
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-    Demo
-        </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
