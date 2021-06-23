a7644ee83baf5e616c95a80d7a6ff55f14fcb818
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-23 13:20:52 -0400

    adding screenshot

--- use_state_initial_demo/src/App.js
+++ use_state_initial_demo/src/App.js
@@ -4,16 +4,18 @@ import React, { useState } from 'react';
 function App() {
 
   // Declare a new state variable, which we'll call "count" & initialize it to 0
-  const [count, setCount] = useState(0);
+  let [count, setCount] = useState(0);
   const initialCount = count;
-  let finalCount =0;
-  if (count==0) {
-    finalCount = count +1;
+  let finalCount =count +1;
+    if (count==0) {// this is needed to ensure setCount is called once only
+    setCount(3);// this changes initialCount to 3 & finalCount to 4 in re render
   }
+    count = 2; // this does not affect initialCount & finalCount
+
   return (
     <div>
       <p>Initially count was {initialCount}.</p>
-      <p>After I assign an incremented count to another variable, final count is: {finalCount}.</p>
+      <p>Final count is: {finalCount}.</p>
       <p>Value of count is {count}.</p>
     </div>
   );
